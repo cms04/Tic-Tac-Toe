69c3a0cd1623893beafa7f0532336d50b3fed4ed
cms04 <Christian.Morbach@hhu.de>
2021-06-12 11:05:29 +0200

    Add field class

---
+++ field.cpp
@@ -0,0 +1,63 @@
+#include "field.h"
+
+Field::Field() {
+    this->reset();
+}
+
+Field::~Field() {
+
+}
+
+void Field::reset() {
+    for (size_t i = 0; i < 3; i++) {
+        for (size_t j = 0; j < 3; j++) {
+            this->data[i][j] = 2;
+        }
+    }
+}
+
+bool Field::setField(size_t y, size_t x, uint8_t p) {
+    if (y < 0 || y > 2 || x < 0 || x > 2 || p != 0 || p != 1) {
+        return false;
+    }
+    if (this->data[y][x] == 2) {
+        return false;
+    }
+    this->data[y][x] = p;
+    return true;
+}
+
+bool Field::check(uint8_t p) {
+    if (p != 0 || p != 1) {
+        return false;
+    }
+    for (size_t i = 0; i < 3; i++) {
+        if (this->data[0][i] == p && this->data[1][i] == p && this->data[2][i] == p) {
+            return true;
+        }
+        if (this->data[i][0] == p && this->data[i][1] == p && this->data[i][2] == p) {
+            return true;
+        }
+    }
+    if (this->data[0][0] == p && this->data[1][1] == p && this->data[2][2] == p) {
+        return true;
+    }
+    if (this->data[2][0] == p && this->data[1][1] == p && this->data[0][2] == p) {
+        return true;
+    }
+    return false;
+}
+
+char Field::getRepresentation(size_t y, size_t x) {
+    if (y < 0 || y > 2 || x < 0 || y > 2) {
+        return '\0';
+    }
+    switch (this->data[y][x]) {
+        case 0:
+            return 'X';
+        case 1:
+            return 'O';
+        default:
+            return ' ';
+    }
+}
